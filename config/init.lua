3dfeeb2adaf4ca66615c7aa9748871ae73bbd1c1
Matheus Bento <matheus.th.bento@gmail.com>
2022-09-25 18:28:14 -0300

    LSP setup

--- config/init.lua
+++ config/init.lua
@@ -1,1 +1,5 @@
+-- Loads packer.nvim plugin configurations setup on plugins.lua
 require('plugins')
+
+-- Setting up python language server
+require('lspconfig').pyright.setup{}
--- config/plugins.lua
+++ config/plugins.lua
@@ -1,3 +1,8 @@
-return require('packer').startup(function(use)
-    use 'wbthomason/packer.nvim'
+packer = require('packer')
+
+return packer.startup(function(use)
+    use 'wbthomason/packer.nvim' -- Package manager
+    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+
+    packer.sync()
 end)
