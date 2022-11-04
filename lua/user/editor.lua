084cd17db7ad2bee57778fb751c4464839e81934
Matheus Bento <matheus.th.bento@gmail.com>
2022-11-04 01:09:11 -0300

    Adding ruler

--- lua/user/editor.lua
+++ lua/user/editor.lua
@@ -18,3 +18,5 @@ vim.o.expandtab       = false    -- Insert spaces instead of tabs
 vim.o.foldmethod     = 'indent' -- Fold following the file indentation
 vim.o.foldlevelstart = 999      -- Setting a high fold level so all blocks are shown on start
 
+vim.opt.colorcolumn = '120' -- Color the 120th column in a different color to use as a ruler
+
